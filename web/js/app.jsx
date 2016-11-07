128452315a27c6d6318a6f8c2a313dc3158bd5e7
Dmitry Gul <guldmitry@gmail.com>
2016-11-07 23:35:25 +0300

    React continue.

--- web/js/app.jsx
+++ web/js/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 var news = [
     {
@@ -24,6 +25,7 @@ class App extends React.Component {
             <div className="app text-primary">
                 <h3> App Component </h3>
                 App component, uses News and Comments inside.
+                <TestInput />
                 <h3> News </h3>
                 <News data={news}/> {/* Comments in JSX are written this way */}
             </div>
@@ -39,23 +41,49 @@ class Article extends React.Component {
         };
     }
 
+    readMoreClick(e) {
+        e.preventDefault();
+        this.setState({visible: true}, () => {
+        });
+    }
+
     render() {
         var author = this.props.data.author;
         var text = this.props.data.text;
         var showMore = this.props.data.showMore;
+        var visible = this.state.visible;
 
         return (
             <div className="article">
                 <p className="news-author">{author}:</p>
                 <p className="news-text">{text}</p>
-                <a href="#" className='news_read-more'>More</a>
-                <p className="news-show-more">{showMore}</p>
+
+                <a href="#"
+                   onClick={(e) => {
+                       this.readMoreClick(e)
+                   }}
+                   className={'news-read-more ' + (visible ? 'hidden' : '')}
+                >
+                    Show More
+                </a>
+                <p className={'news-show-more ' + (visible ? '' : 'hidden')}>{showMore}</p>
             </div>
         )
     }
 }
 
 class News extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0
+        };
+    }
+
+    increaseCounter() {
+        this.setState({counter: ++this.state.counter})
+    }
+
     static propTypes = {
         data: React.PropTypes.arrayOf(React.PropTypes.shape({
             author: React.PropTypes.string.isRequired,
@@ -66,6 +94,7 @@ class News extends React.Component {
 
     render() {
         var data = this.props.data; // <News data={[...]}
+        var counter = this.state.counter;
         var newsTemplate;
 
         if (data.length) {
@@ -84,9 +113,36 @@ class News extends React.Component {
         return (
             <div className="news">
                 <strong className={data.length ? '' : 'hidden'}>News: {data.length}</strong>
+                <p>
+                    <strong onClick={(e) => {
+                        this.increaseCounter()
+                    }}
+                    >
+                        Simple Clicker: {counter}
+                    </strong>
+                </p>
                 {newsTemplate}
             </div>
         );
+    }
+}
+
+class TestInput extends React.Component {
+    onBtnClickHandler() {
+        alert(ReactDOM.findDOMNode(this.refs.myTestInput).value);
+    }
+
+    render() {
+        // Uncontrolled Components. No state => no rerender.
+        return <div>
+            <input
+                className='test-input'
+                defaultValue=''
+                placeholder='Type value'
+                ref='myTestInput'
+            />
+            <button onClick={() => {this.onBtnClickHandler()}} ref='alert-button'>Show</button>
+        </div>
     }
 }
 
