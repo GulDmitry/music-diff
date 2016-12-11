14f00b51f73f662a3f1cf8f18b685d63f44ab882
Dmitry Gul <guldmitry@gmail.com>
2016-12-11 16:48:45 +0300

    Show\Hide artist button.

--- web/js/src/components/Row.js
+++ web/js/src/components/Row.js
@@ -6,8 +6,19 @@ export default class Row extends Component {
         artist: PropTypes.shape({
             albums: PropTypes.array.isRequired,
         }).isRequired,
-        typeFilter: PropTypes.string.isRequired
+        typeFilter: PropTypes.string.isRequired,
     };
+
+    toggleVisibility(e) {
+        const refTokenList = this.refs['table-wrapper'].classList;
+        if (refTokenList.contains('hide')) {
+            refTokenList.remove('hide');
+            e.target.innerText = 'Hide';
+        } else {
+            refTokenList.add('hide');
+            e.target.innerText = 'Show';
+        }
+    }
 
     render() {
         const artist = this.props.artist;
@@ -24,30 +35,40 @@ export default class Row extends Component {
         });
 
         return <div>
-            <h3>{artist.name}</h3>
-            <Table
-                className='table table-hover'
-                sortable={true}
-                defaultSort={{column: 'Release Date', direction: 'desc'}}
-                noDataText='No matching albums found.'
-                filterable={[
-                    {
-                        column: 'Type',
-                        filterFunction: function(contents, filter) {
-                            const data = contents.split(',');
-                            const result = data.filter(function(n) {
-                                return filter.split(',').indexOf(n) > -1;
-                            });
+            <div ref='test' className='row'>
+                <div className='col-md-8'><h3>{artist.name}</h3></div>
+                <h3>
+                    <button onClick={::this.toggleVisibility} type='button' className='btn btn-default btn-sm'>
+                        Hide
+                    </button>
+                </h3>
+            </div>
+            <div ref='table-wrapper'>
+                <Table
+                    ref='row-table'
+                    className='table table-hover'
+                    sortable={true}
+                    defaultSort={{column: 'Release Date', direction: 'desc'}}
+                    noDataText='No matching albums found.'
+                    filterable={[
+                        {
+                            column: 'Type',
+                            filterFunction: function(contents, filter) {
+                                const data = contents.split(',');
+                                const result = data.filter(function(n) {
+                                    return filter.split(',').indexOf(n) > -1;
+                                });
 
-                            return result.length;
+                                return result.length;
+                            }
                         }
-                    }
-                ]}
-                filterBy={this.props.typeFilter}
-                hideFilterInput
-            >
-                {template}
-            </Table>
+                    ]}
+                    filterBy={this.props.typeFilter}
+                    hideFilterInput
+                >
+                    {template}
+                </Table>
+            </div>
         </div>
     }
 }
